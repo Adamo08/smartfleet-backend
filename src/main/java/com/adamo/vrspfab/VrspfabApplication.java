770e3b5b3fa07eeaa5369b91355db398585a39be
Adamo08 <omarelaaddam519@gmail.com>
2025-07-20 18:58:45 +0200

    Initial setup

---
+++ src/main/java/com/adamo/vrspfab/VrspfabApplication.java
@@ -0,0 +1,13 @@
+package com.adamo.vrspfab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrspfabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VrspfabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adamo/vrspfab/VrspfabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamo.vrspfab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrspfabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
